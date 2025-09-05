3689c6d3beb3528f42e08009bbfa3cbe62af3160
Joscha <joscha@plugh.de>
2025-09-05 20:15:33 +0200

    Add repo selector

--- radar-ui/src/App.vue
+++ radar-ui/src/App.vue
@@ -1,34 +1,47 @@
 <script setup lang="ts">
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { useQuery } from "@tanstack/vue-query";
+import * as api from "@/api.ts";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { computed, ref } from "vue";
 
-const { isPending, isError, data, error } = useQuery({
-  queryKey: ["debug"],
-  queryFn: (): Promise<string> =>
-    fetch("/api/debug")
-      .then((it) => it.json())
-      .then((it) => it["text"])
-      .then((it) => {
-        console.log(it);
-        return it;
-      }),
+const repos = useQuery({ queryKey: ["repos"], queryFn: api.getRepos });
+
+const reposInOrder = computed(() => {
+  const repoMap = repos.data.value;
+  if (!repoMap) return [];
+  return [...repoMap.entries()].sort();
 });
+
+const repo = ref<string>();
 </script>
 
 <template>
-  <div class="flex h-dvh items-start justify-center p-6">
-    <Card :class="['w-[300px]']">
-      <CardHeader>
-        <CardTitle>Some text</CardTitle>
-        <CardDescription>... from the server config</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div v-if="isPending" class="text-muted-foreground italic">No text yet</div>
-        <div v-else-if="isError" class="text-destructive-foreground">Error: {{ error }}</div>
-        <div v-else>
-          Text: <span class="italic">{{ data }}</span>
-        </div>
-      </CardContent>
-    </Card>
+  <div class="flex min-h-dvh flex-col">
+    <div class="flex border-b p-2">
+      <Select v-model="repo" :disabled="!repos.isSuccess.value">
+        <SelectTrigger :title="repos.error.value?.message">
+          <SelectValue v-if="repos.isPending.value">Loading repos...</SelectValue>
+          <SelectValue v-else-if="repos.isError.value" title="Hello" class="text-destructive">
+            Error loading repos.
+          </SelectValue>
+          <SelectValue v-else-if="!repo" placeholder="Select a repo"></SelectValue>
+          <SelectValue v-else>{{ repo }}</SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem :value="name" :key="name" v-for="[name, info] of reposInOrder">
+            <div class="flex flex-col">
+              <div>{{ name }}</div>
+              <div class="text-muted-foreground">{{ info.description }}</div>
+            </div>
+          </SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+    <div
+      v-if="!repo"
+      class="text-muted-foreground bg-muted flex grow items-center justify-center text-xl font-light italic"
+    >
+      No repo selected
+    </div>
   </div>
 </template>
---
+++ radar-ui/src/api.ts
@@ -0,0 +1,29 @@
+import * as z from "zod";
+
+async function fetchJson<S extends z.Schema>(url: string, schema: S): Promise<z.infer<S>> {
+  if (!url.startsWith("/")) url = "/" + url;
+
+  const result = await fetch(`/api${url}`);
+  if (!result.ok) {
+    throw new Error(`Failed to fetch ${url}: ${result.statusText}`);
+  }
+
+  const json = await result.json();
+  const parsed = schema.safeParse(json);
+  console.log(json);
+  if (parsed.error) throw new Error(`Failed to fetch ${url}: ${parsed.error.message}`);
+  return parsed.data;
+}
+
+export type JsonRepo = z.infer<typeof JsonRepo>;
+export const JsonRepo = z.object({
+  url: z.url(),
+  description: z.string(),
+});
+
+export async function getRepos(): Promise<Map<string, JsonRepo>> {
+  return await fetchJson(
+    "/repos",
+    z.record(z.string(), JsonRepo).transform((it) => new Map(Object.entries(it))),
+  );
+}
