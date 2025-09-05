06cce82e084667ea94d43d986394e8700672e0ac
Joscha <joscha@plugh.de>
2025-09-05 17:46:38 +0200

    Show debug text

--- radar-ui/src/App.vue
+++ radar-ui/src/App.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
-const x = "Hello world!";
+import { ref } from "vue";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+const text = ref<string>();
+
+async function fetchText(): Promise<void> {
+  await new Promise((r) => setTimeout(r, 1000));
+  const res = await fetch("/api/debug");
+  const data = await res.json();
+  text.value = data["text"];
+}
+
+fetchText();
 </script>
 
 <template>
   <div class="flex h-dvh items-center justify-center">
-    <div class="rounded-full bg-green-200 px-3 py-1">{{ x }}</div>
+    <Card :class="['w-[300px]']">
+      <CardHeader>
+        <CardTitle>Some text</CardTitle>
+        <CardDescription>... from the server config</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div v-if="text === undefined" class="text-muted-foreground italic">No text yet</div>
+        <div v-else>
+          The text is: <span class="italic">{{ text }}</span>
+        </div>
+      </CardContent>
+    </Card>
   </div>
 </template>
