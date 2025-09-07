472eeeeab0f74bbb378968a4d7cb019ba94fdd0d
Joscha <joscha@plugh.de>
2025-09-07 21:02:52 +0200

    Extract repo selector into component

--- radar-ui/src/App.vue
+++ radar-ui/src/App.vue
@@ -1,40 +1,19 @@
 <script setup lang="ts">
-import { useQuery } from "@tanstack/vue-query";
-import * as api from "@/api.ts";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { ref } from "vue";
 import CColorMode from "@/components/CColorMode.vue";
+import CRepoSelect from "@/components/CRepoSelect.vue";
+import { ref } from "vue";
 
-const repos = useQuery({ queryKey: ["repos"], queryFn: api.getRepos });
-
-const selectedRepo = ref<string>();
+const selected = ref<string>();
 </script>
 
 <template>
   <div class="flex min-h-dvh flex-col">
     <div class="flex border-b p-2">
-      <Select v-model="selectedRepo" :disabled="!repos.isSuccess.value">
-        <SelectTrigger :title="repos.error.value?.message">
-          <SelectValue v-if="repos.isPending.value">Loading repos...</SelectValue>
-          <SelectValue v-else-if="repos.isError.value" title="Hello" class="text-destructive">
-            Error loading repos.
-          </SelectValue>
-          <SelectValue v-else-if="!selectedRepo" placeholder="Select a repo"></SelectValue>
-          <SelectValue v-else>{{ selectedRepo }}</SelectValue>
-        </SelectTrigger>
-        <SelectContent v-if="repos.isSuccess.value">
-          <SelectItem :value="repo.name" :key="repo.name" v-for="repo of repos.data.value">
-            <div class="flex flex-col">
-              <div>{{ repo.name }}</div>
-              <div class="text-muted-foreground">{{ repo.description }}</div>
-            </div>
-          </SelectItem>
-        </SelectContent>
-      </Select>
+      <CRepoSelect v-model="selected"></CRepoSelect>
       <div class="flex grow justify-end"><CColorMode></CColorMode></div>
     </div>
     <div
-      v-if="!selectedRepo"
+      v-if="!selected"
       class="text-muted-foreground bg-muted flex grow items-center justify-center text-xl font-light italic"
     >
       No repo selected
---
+++ radar-ui/src/components/CRepoSelect.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useQuery } from "@tanstack/vue-query";
+import * as api from "@/api.ts";
+
+const repos = useQuery({ queryKey: ["repos"], queryFn: api.getRepos });
+const selected = defineModel<string>();
+</script>
+
+<template>
+  <Select v-model="selected" :disabled="!repos.isSuccess.value">
+    <SelectTrigger :title="repos.error.value?.message">
+      <SelectValue v-if="repos.isPending.value">Loading repos...</SelectValue>
+      <SelectValue v-else-if="repos.isError.value" title="Hello" class="text-destructive">
+        Error loading repos.
+      </SelectValue>
+      <SelectValue v-else-if="!selected" placeholder="Select a repo"></SelectValue>
+      <SelectValue v-else>{{ selected }}</SelectValue>
+    </SelectTrigger>
+    <SelectContent v-if="repos.isSuccess.value">
+      <SelectItem :value="repo.name" :key="repo.name" v-for="repo of repos.data.value">
+        <div class="flex flex-col">
+          <div>{{ repo.name }}</div>
+          <div class="text-muted-foreground">{{ repo.description }}</div>
+        </div>
+      </SelectItem>
+    </SelectContent>
+  </Select>
+</template>
+
+<style scoped></style>
