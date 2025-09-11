8b078a46a7bdb00ea9d1c57eabd8917524db5f9f
Joscha <joscha@plugh.de>
2025-09-11 14:15:32 +0200

    Use skeletons for repo overview

---
+++ radar-ui/src/components/ui/CSkeleton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils.ts";
+
+const { class: clazz, isError = false } = defineProps<{
+  isError?: boolean;
+  class?: HTMLAttributes["class"];
+}>();
+</script>
+
+<template>
+  <Skeleton :class="cn(clazz, { 'bg-destructive': isError })" />
+</template>
+
+<style scoped></style>
--- radar-ui/src/pages/repos.[repo].vue
+++ radar-ui/src/pages/repos.[repo].vue
@@ -1,28 +1,40 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
-import { useRepoInfo } from "@/composables/useRepoInfo.ts";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { GitHubIcon } from "vue3-simple-icons";
 import { useRepoHistory } from "@/composables/useRepoHistory.ts";
-import { reactive } from "vue";
+import { computed, reactive } from "vue";
 import { useDateFormat, useTimeAgo } from "@vueuse/core";
-import { GitCommitVerticalIcon, EllipsisVerticalIcon } from "lucide-vue-next";
+import { EllipsisVerticalIcon, GitCommitVerticalIcon } from "lucide-vue-next";
+import { useRepos } from "@/composables/useRepos.ts";
+import CSkeleton from "@/components/ui/CSkeleton.vue";
 
 const route = useRoute("/repos.[repo]");
-const info = useRepoInfo(() => route.params.repo);
+const repos = reactive(useRepos());
 const history = reactive(useRepoHistory(() => route.params.repo));
+
+const info = computed(() => repos.data?.repos.find((it) => it.name === route.params.repo));
 </script>
 
 <template>
   <Card>
     <CardHeader>
-      <CardTitle>{{ route.params.repo }}</CardTitle>
-      <CardDescription>{{ info?.description ?? "This repo does not exist." }}</CardDescription>
+      <CardTitle>
+        <CSkeleton v-if="!repos.isSuccess" :is-error="repos.isError" class="h-4 w-[8ch]" />
+        <template v-else>{{ route.params.repo }}</template>
+      </CardTitle>
+      <CardDescription>
+        <CSkeleton v-if="!repos.isSuccess" :is-error="repos.isError" class="my-1 h-3 w-[24ch] pt-2" />
+        <template v-else-if="info">{{ info.description }}</template>
+        <template v-else>This repo does not exist.</template>
+      </CardDescription>
     </CardHeader>
     <CardContent>
       <div class="flex flex-col items-start">
+        <template v-if="repos.isSuccess && !info" />
+        <CSkeleton v-else-if="!repos.isSuccess || !info" :is-error="repos.isError" class="h-7 w-[30ch] border" />
         <a
-          v-if="info !== undefined"
+          v-else
           class="flex gap-2 rounded-md border px-2 py-1 text-sm hover:underline"
           :href="info.url"
           target="_blank"
@@ -34,13 +46,19 @@ const history = reactive(useRepoHistory(() => route.params.repo));
     </CardContent>
   </Card>
 
-  <Card>
+  <Card v-if="!(repos.isSuccess && !info)">
     <CardHeader>
       <CardTitle>Recent commits</CardTitle>
       <CardDescription>Freshly cherry-picked</CardDescription>
     </CardHeader>
     <CardContent>
-      <div v-if="!history.isSuccess" class="text-muted-foreground italic">Loading...</div>
+      <div v-if="!history.isSuccess" class="flex items-center gap-2">
+        <CSkeleton :is-error="history.isError" class="h-8 w-8 rounded-full" />
+        <div class="flex flex-col">
+          <CSkeleton :is-error="history.isError" class="my-1 h-4 w-[30ch]" />
+          <CSkeleton :is-error="history.isError" class="my-1 h-3 w-[20ch]" />
+        </div>
+      </div>
       <div v-else class="flex flex-col gap-4">
         <RouterLink
           :to="{ name: '/repos.[repo].commits.[chash]', params: { repo: route.params.repo, chash: commit.chash } }"
