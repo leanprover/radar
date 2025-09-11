996db42cb8fb2fdc7498a53495bbed6171d4d6f8
Joscha <joscha@plugh.de>
2025-09-11 16:31:47 +0200

    Tweak commit view design

--- radar-ui/src/pages/repos.[repo].commits.[chash].vue
+++ radar-ui/src/pages/repos.[repo].commits.[chash].vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { useRoute } from "vue-router";
 import { reactive } from "vue";
 import { useCommitInfo } from "@/composables/useCommitInfo.ts";
@@ -18,57 +18,59 @@ const commit = reactive(
 </script>
 
 <template>
-  <div class="flex flex-col gap-2">
-    <Card>
-      <CardHeader>
-        <CardTitle>
-          <CSkeleton v-if="!commit.isSuccess" :error="commit.error" class="h-4 w-[30ch]" />
-          <template v-else>{{ commit.data.title }}</template>
-        </CardTitle>
-        <CardDescription>
-          <CSkeleton v-if="!commit.isSuccess" :error="commit.error" class="my-1 h-3 w-[24ch] pt-2" />
-          <template v-else>
-            <span :title="useDateFormat(commit.data.committer.time, 'YYYY-MM-DD HH:mm:ss').value">
-              {{ useTimeAgo(commit.data.committer.time) }}
-            </span>
-            by {{ commit.data.author.name }} &lt;{{ commit.data.author.email }}&gt;
-          </template>
-        </CardDescription>
-      </CardHeader>
-      <CardContent class="flex flex-col gap-2">
-        <CSkeleton v-if="!commit.isSuccess" :error="commit.error" class="h-[100px] w-[80ch]" />
-        <pre class="text-sm whitespace-pre-wrap" v-else-if="commit.data.body">{{ commit.data.body }}</pre>
-      </CardContent>
-    </Card>
-    <div class="flex gap-8" v-if="commit.isSuccess">
-      <div class="flex flex-1 flex-col items-start gap-1">
-        <RouterLink
-          v-for="commit in commit.data.parents"
-          :to="{ name: '/repos.[repo].commits.[chash]', params: { repo: route.params.repo, chash: commit.chash } }"
-          :class="
-            cn('bg-background flex items-center rounded-md border pr-2 hover:underline', {
-              'text-muted-foreground': !commit.tracked,
-            })
-          "
-        >
-          <ChevronLeftIcon />
-          <span class="line-clamp-1 text-sm break-all">{{ commit.title }}</span>
-        </RouterLink>
+  <Card>
+    <CardHeader>
+      <CardTitle>
+        <CSkeleton v-if="!commit.isSuccess" :error="commit.error" class="h-4 w-[30ch]" />
+        <template v-else>{{ commit.data.title }}</template>
+      </CardTitle>
+      <CardDescription>
+        <CSkeleton v-if="!commit.isSuccess" :error="commit.error" class="my-1 h-3 w-[24ch] pt-2" />
+        <template v-else>
+          <span :title="useDateFormat(commit.data.committer.time, 'YYYY-MM-DD HH:mm:ss').value">
+            {{ useTimeAgo(commit.data.committer.time) }}
+          </span>
+          by {{ commit.data.author.name }} &lt;{{ commit.data.author.email }}&gt;
+        </template>
+      </CardDescription>
+    </CardHeader>
+    <CardContent v-if="!(commit.isSuccess && !commit.data.body)" class="flex flex-col gap-2">
+      <CSkeleton v-if="!commit.isSuccess" :error="commit.error" class="h-[100px] w-[80ch]" />
+      <pre class="border-l-4 py-1 pl-3 text-sm whitespace-pre-wrap" v-else-if="commit.data.body">{{
+        commit.data.body
+      }}</pre>
+    </CardContent>
+    <CardFooter class="flex flex-col items-stretch">
+      <div class="flex gap-8" v-if="commit.isSuccess">
+        <div class="flex flex-1 flex-col items-start gap-1">
+          <RouterLink
+            v-for="commit in commit.data.parents"
+            :to="{ name: '/repos.[repo].commits.[chash]', params: { repo: route.params.repo, chash: commit.chash } }"
+            :class="
+              cn('flex hover:underline', {
+                'text-muted-foreground': !commit.tracked,
+              })
+            "
+          >
+            <ChevronLeftIcon :size="20" class="shrink-0" />
+            <div class="line-clamp-1 text-sm break-all">{{ commit.title }}</div>
+          </RouterLink>
+        </div>
+        <div class="flex flex-1 flex-col items-end gap-1">
+          <RouterLink
+            v-for="commit in commit.data.children"
+            :to="{ name: '/repos.[repo].commits.[chash]', params: { repo: route.params.repo, chash: commit.chash } }"
+            :class="
+              cn('flex pl-2 hover:underline', {
+                'text-muted-foreground': !commit.tracked,
+              })
+            "
+          >
+            <div class="line-clamp-1 text-sm break-all">{{ commit.title }}</div>
+            <ChevronRightIcon :size="20" class="shrink-0" />
+          </RouterLink>
+        </div>
       </div>
-      <div class="flex flex-1 flex-col items-end gap-1">
-        <RouterLink
-          v-for="commit in commit.data.children"
-          :to="{ name: '/repos.[repo].commits.[chash]', params: { repo: route.params.repo, chash: commit.chash } }"
-          :class="
-            cn('bg-background flex items-center rounded-md border pl-2 hover:underline', {
-              'text-muted-foreground': !commit.tracked,
-            })
-          "
-        >
-          <span class="line-clamp-1 text-sm break-all">{{ commit.title }}</span>
-          <ChevronRightIcon />
-        </RouterLink>
-      </div>
-    </div>
-  </div>
+    </CardFooter>
+  </Card>
 </template>
