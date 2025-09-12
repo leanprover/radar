39db734e0f360fc4ab565ca65568b897b007421f
Joscha <joscha@plugh.de>
2025-09-12 15:28:51 +0200

    Remove unnecessary revify logic
    Jgit's resolve command already resolves refs as one might expect, and I
    see no reason to prevent users from using different kinds of refs.

--- radar/src/main/java/org/leanlang/radar/server/config/ServerConfigRepo.java
+++ radar/src/main/java/org/leanlang/radar/server/config/ServerConfigRepo.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotNull;
 import java.net.URI;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import org.checkerframework.checker.nonempty.qual.NonEmpty;
 
 public record ServerConfigRepo(
@@ -15,18 +14,4 @@ public record ServerConfigRepo(
         @NotEmpty Set<@NonEmpty String> track,
         @NotNull URI benchUrl,
         @NotEmpty String benchRef,
-        @NotEmpty List<ServerConfigRepoRun> benchRuns) {
-
-    private static String refify(String refName) {
-        if (refName.startsWith("ref/")) return refName;
-        return "refs/heads/" + refName;
-    }
-
-    public Set<String> track() {
-        return track.stream().map(ServerConfigRepo::refify).collect(Collectors.toUnmodifiableSet());
-    }
-
-    public String benchRef() {
-        return refify(benchRef);
-    }
-}
+        @NotEmpty List<ServerConfigRepoRun> benchRuns) {}
