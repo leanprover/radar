7c658879578b70c57c1cc8139fcffed7a5e00ca4
Joscha <joscha@plugh.de>
2025-09-16 13:09:09 +0200

    Remove unused server tmp dir config option

--- radar/src/main/java/org/leanlang/radar/server/config/Dirs.java
+++ radar/src/main/java/org/leanlang/radar/server/config/Dirs.java
@@ -5,13 +5,11 @@ import java.nio.file.Path;
 public final class Dirs {
     private final Path data;
     private final Path cache;
-    private final Path tmp;
 
     public Dirs(Path configFile, ServerConfigDirs dirs) {
         Path root = configFile.getParent();
         data = root.resolve(dirs.data());
         cache = root.resolve(dirs.cache());
-        tmp = root.resolve(dirs.tmp());
     }
 
     public Path repoDb(String repo) {
