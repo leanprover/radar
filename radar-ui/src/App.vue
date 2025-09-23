63268f57cf3f2ea2a1f29d3e6b39aae5e3fc4a68
Joscha <joscha@plugh.de>
2025-09-23 16:20:24 +0200

    Get rid of shadcn/vue

--- radar-ui/src/App.vue
+++ radar-ui/src/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 import CNavbar from "@/components/CNavbar.vue";
-import { TooltipProvider } from "@/components/ui/tooltip";
 </script>
 
 <template>
-  <TooltipProvider>
-    <div class="flex flex-col">
-      <CNavbar />
-      <div class="flex grow flex-col gap-8 p-2 pt-4">
-        <RouterView />
-      </div>
+  <div class="flex flex-col">
+    <CNavbar />
+    <div class="flex grow flex-col gap-8 p-2 pt-4">
+      <RouterView />
     </div>
-  </TooltipProvider>
+  </div>
 </template>
