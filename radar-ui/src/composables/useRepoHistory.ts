405570dcd8417b26a6e27ff885077efb430e4084
Joscha <joscha@plugh.de>
2025-10-02 17:09:49 +0200

    Rename file for consistency

--- radar-ui/src/composables/useRepoHistory.ts
+++ radar-ui/src/composables/useRepoHistory.ts
@@ -1,7 +1,7 @@
 import { toValue } from "vue";
 import type { MaybeRefOrGetter } from "@vueuse/core";
 import { useQuery } from "@tanstack/vue-query";
-import { getRepoHistory } from "@/api/reposHistory.ts";
+import { getRepoHistory } from "@/api/repoHistory.ts";
 
 export function useRepoHistory(repo: MaybeRefOrGetter<string>) {
   return useQuery({ queryKey: ["repoHistory", { repo }], queryFn: () => getRepoHistory(toValue(repo)) });
