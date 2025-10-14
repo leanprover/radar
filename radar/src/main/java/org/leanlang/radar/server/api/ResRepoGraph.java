23b417af6228c83688082c969ce0a519cea886f2
Joscha <joscha@plugh.de>
2025-10-14 14:37:19 +0200

    Increase graph endpoint limits

--- radar/src/main/java/org/leanlang/radar/server/api/ResRepoGraph.java
+++ radar/src/main/java/org/leanlang/radar/server/api/ResRepoGraph.java
@@ -36,8 +36,8 @@ public record ResRepoGraph(Repos repos) {
     @Produces(MediaType.APPLICATION_JSON)
     public JsonGet get(@PathParam("repo") String name, @QueryParam("m") List<String> metrics, @QueryParam("n") int n) {
         Repo repo = repos.repo(name);
-        if (metrics.size() > 10) throw new BadRequestException("too many metrics");
-        if (n > 1000) throw new BadRequestException("n too large");
+        if (metrics.size() > 100) throw new BadRequestException("too many metrics");
+        if (n > 10000) throw new BadRequestException("n too large");
 
         return repo.db()
                 .readTransactionResult(ctx -> {
