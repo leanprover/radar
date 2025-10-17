7eac0cdba853b3005cc30fd7c39386930133e7fc
Joscha <joscha@plugh.de>
2025-10-17 17:58:57 +0200

    Prevent fractional x axis ticks

--- radar-ui/src/components/pages/graph/PUplot.vue
+++ radar-ui/src/components/pages/graph/PUplot.vue
@@ -22,7 +22,10 @@ const options = computed<uPlot.Options>(() => ({
   height: defaultWidth / aspectRatio,
 
   axes: [
-    {},
+    {
+      // Prevent fractional increments (there is no 0.1 commits)
+      incrs: [1, 2, 5, 10, 20, 50, 100, 200, 500],
+    },
     {
       size: 70,
       // TODO Format values with respect to y axis range
