3cc34b45cfcb205984e79fd0b5104f9db39c8930
Joscha <joscha@plugh.de>
2025-10-29 11:58:55 +0100

    Fix db transaction accidentally deleting all measurements

--- radar/src/main/resources/db/migration/V7__Cleanup.sql
+++ radar/src/main/resources/db/migration/V7__Cleanup.sql
@@ -3,11 +3,28 @@ CREATE TABLE metrics_new (
     unit   TEXT
 ) STRICT;
 
+CREATE TABLE measurements_new (
+    chash  TEXT NOT NULL,
+    metric TEXT NOT NULL,
+    value  REAL NOT NULL,
+    source TEXT,
+    PRIMARY KEY (chash, metric),
+    FOREIGN KEY (chash) REFERENCES commits (chash) ON DELETE CASCADE,
+    FOREIGN KEY (metric) REFERENCES metrics_new (metric) ON DELETE CASCADE
+) STRICT;
+
 INSERT INTO metrics_new
 SELECT metric, unit
 FROM metrics;
 
+INSERT INTO measurements_new
+SELECT chash, metric, value, source
+FROM measurements;
+
+DROP TABLE measurements;
 DROP TABLE metrics;
 
 ALTER TABLE metrics_new
     RENAME TO metrics;
+ALTER TABLE measurements_new
+    RENAME TO measurements;
