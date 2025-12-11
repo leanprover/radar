2373f15864f3569b09c7e6ecda0a5839cbe92eae
Joscha <joscha@plugh.de>
2025-12-11 23:06:35 +0100

    TMP experiment with some library

--- scripts/analysis/analyze.py
+++ scripts/analysis/analyze.py
@@ -3,27 +3,32 @@ import json
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
+import ruptures as rpt
 
 
-def mean(values: list[float]) -> float:
+def compute_mean(values: list[float]) -> float:
     assert values, "need at least one value for mean"
     return sum(values) / len(values)
 
 
-def variance(values: list[float]) -> float:
+# Expects sorted values
+def compute_variance(values: list[float]) -> float:
     assert len(values) >= 2, "need at least two values for variance"
     n = len(values)
-    mu = mean(values)
+    mu = compute_mean(values)
     return sum((x - mu) ** 2 for x in values) / (n - 1)
 
 
-def stddev(values: list[float]) -> float:
+# Expects sorted values
+def compute_stddev(values: list[float]) -> float:
     # Not completely unbiased, but good enough
     assert len(values) >= 2, "need at least two values for stddev"
-    return variance(values) ** 0.5
+    return compute_variance(values) ** 0.5
 
 
-def quantile(values: list[float], p: float) -> float:
+# Expects sorted values
+def compute_quantile(values: list[float], p: float) -> float:
     # https://en.wikipedia.org/wiki/Percentile#Third_variant,_C_=_0
     assert values, "need at least one value for percentile"
     n = len(values)
@@ -38,12 +43,26 @@ def quantile(values: list[float], p: float) -> float:
     return values[lower - 1] * (1 - weight) + values[upper - 1] * weight
 
 
+def smoothen(values: list[float], window: int = 3) -> list[float]:
+    result = []
+    for i in range(len(values) - window + 1):
+        window_values = values[i : i + window]
+        window_mean = compute_mean(window_values)
+        result.append(window_mean)
+    return result
+
+
+def differentiate(values: list[float]) -> list[float]:
+    return [b - a for a, b in zip(values, values[1:])]
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("data", type=Path)
     args = parser.parse_args()
 
     datafile: Path = args.data
+    outdir: Path = datafile.with_suffix(".out")
 
     print("Loading data...", end="", flush=True)
     with datafile.open() as f:
@@ -57,6 +76,12 @@ def main():
     metrics = {m for c in by_sha.values() for m in c}
     print(f" Found {len(metrics)} metrics.")
 
+    print("Preparing outdir...", end="", flush=True)
+    outdir.mkdir(parents=True, exist_ok=True)
+    for child in outdir.iterdir():
+        child.unlink()
+    print(" Done.")
+
     print("Analyzing data...")
 
     by_metric = {
@@ -64,75 +89,31 @@ def main():
         for metric in metrics
     }
 
-    for metric in sorted(metrics):
+    w = len(str(len(metrics)))
+    for i, metric in enumerate(sorted(metrics), start=1):
         topic, category = metric.split("//")
         name = f"{category.replace('/', '|')}||{topic.replace('/', '|')}"
-        p = Path() / "out" / f"{name}.png"
+        p = outdir / f"{name}.png"
+
         values = by_metric[metric]
         values.reverse()  # Old to new
-        deltas = [b - a for a, b in zip(values, values[1:])]
-
         if len(values) < 10:
             continue
 
-        v_mean = mean(sorted(values))
-        v_stddev = stddev(sorted(values))
-        v_z_scores = [(v - v_mean) / v_stddev for v in values if v_stddev != 0]
+        algo = rpt.Pelt(model="rbf").fit(np.array(values))
+        result = algo.predict(pen=10)
 
-        v_q1 = quantile(sorted(values), 0.25)
-        v_q3 = quantile(sorted(values), 0.75)
-        v_q_lower = v_q1 - (1.5 * (v_q3 - v_q1))
-        v_q_upper = v_q3 + (1.5 * (v_q3 - v_q1))
-
-        d_mean = mean(sorted(deltas))
-        d_stddev = stddev(sorted(deltas))
-        # d_z_scores = [(d - d_mean) / d_stddev for d in deltas if v_stddev != 0]
-        d_z_scores = [d / d_stddev for d in deltas if v_stddev != 0]
-
-        d_q1 = quantile(sorted(deltas), 0.25)
-        d_q3 = quantile(sorted(deltas), 0.75)
-        d_q_lower = d_q1 - (1.5 * (d_q3 - d_q1))
-        d_q_upper = d_q3 + (1.5 * (d_q3 - d_q1))
-
-        plt.figure(figsize=(16, 16))
-        plt.suptitle(metric)
-
-        plt.subplot(2, 2, 1)
-        plt.scatter(range(len(v_z_scores)), v_z_scores, alpha=0.5)
-        plt.title("z-scores")
-        plt.axhline(0, color="red", linestyle="--")
+        plt.figure(figsize=(16, 8))
+        plt.title("values")
         plt.grid(True)
-        plt.ylim(-5, 5)
-
-        plt.subplot(2, 2, 2)
-        plt.scatter(range(len(values)), values, alpha=0.5)
-        plt.title("quartile-normalized")
-        plt.axhline(v_q1, color="blue", linestyle="--")
-        plt.axhline(v_q3, color="blue", linestyle="--")
-        plt.axhline(v_q_lower, color="red", linestyle="--")
-        plt.axhline(v_q_upper, color="red", linestyle="--")
-        plt.grid(True)
-
-        plt.subplot(2, 2, 3)
-        plt.scatter(range(len(d_z_scores)), d_z_scores, alpha=0.5)
-        plt.title("delta z-scores")
-        plt.axhline(0, color="red", linestyle="--")
-        plt.grid(True)
-        plt.ylim(-5, 5)
-
-        plt.subplot(2, 2, 4)
-        plt.scatter(range(len(deltas)), deltas, alpha=0.5)
-        plt.title("quartile-normalized deltas")
-        plt.axhline(d_q1, color="blue", linestyle="--")
-        plt.axhline(d_q3, color="blue", linestyle="--")
-        plt.axhline(d_q_lower, color="red", linestyle="--")
-        plt.axhline(d_q_upper, color="red", linestyle="--")
-        plt.grid(True)
+        plt.plot(range(len(values)), values, linestyle="-")
+        for cp in result:
+            plt.axvline(cp, color="red", linestyle="--")
 
         p.parent.mkdir(parents=True, exist_ok=True)
         plt.savefig(p)
         plt.close()
-        print(f"Saved plots for {metric} to {p}")
+        print(f"[{i:{w}}/{len(metrics)}] {metric} to {p}")
 
 
 if __name__ == "__main__":
