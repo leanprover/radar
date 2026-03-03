4e996a2519c75057edcf544e1acf18823935c598
Joscha <joscha@plugh.de>
2026-03-03 15:39:03 +0100

    foo7

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 
-# Limit memory (all values are KiB
+# Limit memory (all values are KiB)
 mem_total="$(awk '/MemTotal/ { print $2 }' /proc/meminfo)"
 mem_limit="$((mem_total * 90 / 100))"
 ulimit -v "$mem_limit"
 
+timeout -s kill 1h bash -c '
 scripts/fmt
 scripts/build
+'
