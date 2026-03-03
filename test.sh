8b64dd40c5769e4a700d94880e363da66b891361
Joscha <joscha@plugh.de>
2026-03-03 14:51:08 +0100

    foo2

--- test.sh
+++ test.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 set -eux
 
-cat /proc/self/cgroup | grep -Po '(?<=^0::).*$'
+# Compute memory limit in bytes
+mem_total="$(awk '/MemTotal/ { print $2 * 1024 }' /proc/meminfo)"
+mem_limit="$((mem_total * 90 / 100))"
+
+# Create cgroup
+cg_main="/sys/fs/cgroup$(awk -F '::' '{ print $2 }' /proc/self/cgroup)"
+cg_bench="$(dirname "$cg_main")/bench"
+mkdir "$cg_bench"
+echo "$mem_limit" > "$cg_bench/memory.max"
+
+(
+  # Run something in the newly created cgroup
+  echo $$ > "$cg_bench/cgroup.procs"
+  echo "subshell cgroup is:"
+  cat /proc/self/cgroup
+)
