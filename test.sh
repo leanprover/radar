b5d8fae28c741b780680c0b36c5a9578d57d5bda
Joscha <joscha@plugh.de>
2026-03-03 15:18:07 +0100

    foo3

--- test.sh
+++ test.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 set -eux
 
-# Compute memory limit in bytes
-mem_total="$(awk '/MemTotal/ { print $2 * 1024 }' /proc/meminfo)"
-mem_limit="$((mem_total * 90 / 100))"
+systemd-run --scope -- echo hi
 
-# Create cgroup
-cg_main="/sys/fs/cgroup$(awk -F '::' '{ print $2 }' /proc/self/cgroup)"
-cg_bench="$(dirname "$cg_main")/bench"
-mkdir "$cg_bench"
-echo "$mem_limit" > "$cg_bench/memory.max"
-
-(
-  # Run something in the newly created cgroup
-  echo $$ > "$cg_bench/cgroup.procs"
-  echo "subshell cgroup is:"
-  cat /proc/self/cgroup
-)
+## Compute memory limit in bytes
+#mem_total="$(awk '/MemTotal/ { print $2 * 1024 }' /proc/meminfo)"
+#mem_limit="$((mem_total * 90 / 100))"
+#
+## Create cgroup
+#cg_main="/sys/fs/cgroup$(awk -F '::' '{ print $2 }' /proc/self/cgroup)"
+#cg_bench="$(dirname "$cg_main")/bench"
+#mkdir "$cg_bench"
+#echo "$mem_limit" > "$cg_bench/memory.max"
+#
+#(
+#  # Run something in the newly created cgroup
+#  echo $$ > "$cg_bench/cgroup.procs"
+#  echo "subshell cgroup is:"
+#  cat /proc/self/cgroup
+#)
