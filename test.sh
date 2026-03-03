7efe4e22c100a84e35c6d778032195ba058897a3
Joscha <joscha@plugh.de>
2026-03-03 15:31:58 +0100

    foo6

--- test.sh
+++ test.sh
@@ -1,21 +1,10 @@
 #!/usr/bin/env bash
 set -eux
 
-systemd-run -- echo hi
+# Limit memory (all values are KiB
+mem_total="$(awk '/MemTotal/ { print $2 }' /proc/meminfo)"
+mem_limit="$((mem_total * 90 / 100))"
+ulimit -v "$mem_limit"
 
-## Compute memory limit in bytes
-#mem_total="$(awk '/MemTotal/ { print $2 * 1024 }' /proc/meminfo)"
-#mem_limit="$((mem_total * 90 / 100))"
-#
-## Create cgroup
-#cg_main="/sys/fs/cgroup$(awk -F '::' '{ print $2 }' /proc/self/cgroup)"
-#cg_bench="$(dirname "$cg_main")/bench"
-#mkdir "$cg_bench"
-#echo "$mem_limit" > "$cg_bench/memory.max"
-#
-#(
-#  # Run something in the newly created cgroup
-#  echo $$ > "$cg_bench/cgroup.procs"
-#  echo "subshell cgroup is:"
-#  cat /proc/self/cgroup
-#)
+scripts/fmt
+scripts/build
